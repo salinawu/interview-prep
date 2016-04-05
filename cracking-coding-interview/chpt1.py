261d65e5f271fb44554da9ab54ec05c8dac81fb8
Salina Wu <salinawu@uchicago.edu>
2016-04-05 17:03:17 +0200

    q2

--- cracking-coding-interview/chpt1.py
+++ cracking-coding-interview/chpt1.py
@@ -1,4 +1,4 @@
-# 1. Implement an algorithm to determine if a string has all unique characters
+# 1.1 Implement an algorithm to determine if a string has all unique characters
 # What if you can not use additional data structures?
 
 
@@ -33,3 +33,40 @@ def unique_string1(s):
 #
 # print(unique_string('sss'))
 # print(unique_string('abcd1029'))
+
+
+# 1.2 Write code to reverse a C-Style String
+
+def reverse_cstring(s):
+    r = s[:-1]
+    new = []
+    for i in reversed(r):
+        new.append(i)
+
+    return new
+
+#print(reverse_cstring("salina"))
+
+# 1.3 Design an algorithm and write code to remove the duplicate characters
+# in a string without using any additional buffer
+
+def dup_char(s):
+    print(s)
+    s = sorted(s)
+    print(s)
+    new = []
+    a = s[0]
+    print(s[1:])
+    for i in s[1:]:
+
+        if a == i:
+            s.remove(a)
+        else:
+            # print("i:" + i)
+            # print(a)
+            new.append(a)
+
+            a = i
+    return new
+
+print(dup_char("salina"))
