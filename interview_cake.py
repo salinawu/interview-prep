96fba13f9ddfe95a0b001d918d2a38647fae4ee1
Salina Wu <salinawu@uchicago.edu>
2016-11-29 00:02:31 -0600

    welp gonna fail

--- interview_cake.py
+++ interview_cake.py
@@ -93,4 +93,107 @@ def denominations(denoms, n):
                 table[i][j] = with_m + without_m
     return table[n][index-1]
 
-print denominations([2, 5, 3, 6], 10)
+#print denominations([2, 5, 3, 6], 10)
+
+class Rectangle(object):
+    def __init__(self, x, y, w, h):
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+
+    def right_x(self):
+        return self.x + self.w
+
+    def up_y(self):
+        return self.y + self.h
+
+    def intersection(self, other):
+        min_x = self if self.x < other.x else other
+        other_x = self if min_x!= self else other
+        if min_x.right_x() < other_x.x:
+            return False
+
+        min_y = self if self.y < other.y else other
+        other_y = self if min_y!= self else other
+        if min_y.up_y() < other.y:
+            return False
+
+        x = min(self.right_x(), other.right_x())
+        y = min(self.up_y(), other.up_y())
+        return Rectangle(other_x.x, other_y.y, x-other_x.x, y-other_y.y)
+
+    def print_rectangle(self):
+        print [self.x, self.y, self.w, self.h]
+
+r1 = Rectangle(1, 1, 5, 2)
+r2 = Rectangle(4, 2, 3, 5)
+#r2.intersection(r1).print_rectangle()
+
+def check_validity(root, min, max):
+    if not root:
+        return True
+    elif root.val >= min and root.val <= max:
+        return check_validity(root.l, min, root.val) and check_validity(root.r, root.val, max)
+    else:
+        return False
+
+class Tree(object):
+    def __init__(self, val):
+        self.val = val
+        self.l = None
+        self.r = None
+
+    def isBalanced(self):
+        return self.balance() > 0
+
+    def balance(self):
+        if not self:
+            return 0
+        left_balanced = left.balance()
+        right_balanced = right.balance()
+        if left_balanced < 0 or right_balanced < 0 or abs(left_balanced - right_balanced)>1:
+            return -1
+        return 1 + max(left_balanced, right_balanced)
+
+    def validBST(self):
+        return check_validity(self, float('-inf'), float('inf'))
+
+
+def exist(board, word):
+    visited = [[0 for i in board[0]] for j in board]
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            if not visited[i][j]:
+                if board[i][j] in word:
+                    letters = []
+                    add_letters(board, i, j, letters, word, visited)
+                    if word in letters:
+                        return True
+    return False
+
+    """
+    :type board: List[List[str]]
+    :type word: str
+    :rtype: bool
+    """
+
+def add_letters(board, s, e, letters, word, visited):
+    if 0 > s or s >= len(board) or 0 > e or e >= len(board[0]) or visited[s][e]:
+        return
+    #print [s, e]
+    l = board[s][e]
+    visited[s][e] = 1
+    if l in word:
+        letters.append(l)
+        x = s+1
+        y = e+1
+        print [s-1, e]
+        add_letters(board, s-1, e, letters, word, visited)
+        add_letters(board, s+1, e, letters, word, visited)
+        add_letters(board, s, e-1, letters, word, visited)
+        add_letters(board, s, e+1, letters, word, visited)
+    else:
+        return
+
+print exist(["AB","SF","AD"], "ABS")
